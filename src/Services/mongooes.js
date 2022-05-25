c65dfd882832db99dcdaa589683952785fc62004
pk prajapati <pappu.kumar@fluper.in>
2022-05-25 17:56:43 +0530

    change url

--- src/Services/mongooes.js
+++ src/Services/mongooes.js
@@ -1,10 +1,11 @@
 
 const mongoose = require('mongoose');
 
-// const conn = mongoose.connect('mongodb+srv://evapiUser:FMwsbw1Msc9gaqPg@cluster0.t8rny.mongodb.net/evapiDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, });
+//  const conn = mongoose.connect('mongodb://cosmicuser:cosmicT0011@15.207.219.174:27017/cosmic_db', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false, useCreateIndex:true }); 
+
 
 //for server
- const conn = mongoose.connect('mongodb://cosmicuser:cosmicT0011@15.207.219.174:27017/cosmic_db', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false, useCreateIndex:true }); // for local
+ const conn = mongoose.connect('mongodb://cosmicuser:cosmicT0011@127.0.0.1:27017/cosmic_db', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false, useCreateIndex:true }); // for local
 
 exports.mongoose = mongoose;
 
